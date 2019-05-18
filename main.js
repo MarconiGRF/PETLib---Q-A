a602aafe74e70dcd1613fb6c20c41fb3f192e827
Marconi Gomes <mgrf@cin.ufpe.br>
2019-05-18 01:07:04 -0300

    Finalizado o esqueleto mínimo.

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 600,
-    height: 870,
+    height: 900,
     webPreferences: {
       nodeIntegration: true
     }
